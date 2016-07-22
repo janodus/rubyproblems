0ffde2c2c653a6da21bea1d1c8b736419e5eecb3
Jan  Viloria <MainAir@Jans-MacBook-Air.local>
2016-07-21 18:15:00 -0700

    ZZ PRACTICE TEST - DISEMVOWEL

---
+++ disemvowel.rb
@@ -0,0 +1,17 @@
+=begin Write a function disemvowel(string), which takes in a string, and returns that string with all the vowels removed. Treat "y" as a consonant.
+
+EXPLANATION:
+A single line solution to removing all vowels in a given string, and returning the remaining characters. The #puts is there to test on console. 
+
+=end
+
+def disemvowel(string)
+
+puts string.delete("aeiou")
+return string.delete("aeiou")
+
+end
+
+disemvowel("foobar") == "fbr"
+disemvowel("ruby") == "rby"
+disemvowel("aeiou") == ""
